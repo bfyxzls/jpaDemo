7b29b87bd8e00d6ad6a20ffe262a20624f8ca056
lind.zhang <bfyxzls@sina.com>
2018-07-19 15:17:23 +0800

    mysql运行成功

--- src/main/java/javalindday/jpaDemo/controller/HomeController.java
+++ src/main/java/javalindday/jpaDemo/controller/HomeController.java
@@ -1,13 +1,16 @@
 package javalindday.jpaDemo.controller;
 
+import java.util.Date;
 import java.util.List;
 import javalindday.jpaDemo.model.UserInfo;
 import javalindday.jpaDemo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/home")
 public class HomeController {
@@ -24,5 +27,11 @@ public class HomeController {
   public String index(String name, String password) {
     userService.createNewAccount(name, password, 100);
     return "OK";
+  }
+
+  @GetMapping("logger")
+  public String log() {
+    log.info("控制台日志 {}", new Date());
+    return "logger";
   }
 }
